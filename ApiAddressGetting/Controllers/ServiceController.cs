9f1102f9517072b60429f507e3031b4b234a3a91
Fehmin1905 <70522742+Fehmin1905@users.noreply.github.com>
2021-11-26 18:34:37 +0400

    First commit

---
+++ ApiAddressGetting/Controllers/ServiceController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiAddressGetting.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string ClientIPAddress { get; private set; }
+
+
+        public IActionResult GetIpAddress()
+        {
+
+            ClientIPAddress = HttpContext.Connection.RemoteIpAddress?.ToString();
+            ViewBag.IpAddress = ClientIPAddress;
+            return View();
+        }
+
+        public IActionResult GetPageInformation()
+        {
+            var url = HttpContext.Request.GetEncodedUrl();
+            ViewBag.GetPageInfo = url;
+            return View();
+        }
+    }
+}
